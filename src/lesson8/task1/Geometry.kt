9868320a41752908b38f3d97004384a78725f723
tengen-toppa-gurren-lagann <un.al@mail.ru>
2019-12-18 21:56:59 +0300

    1-5 lessons fixed, 7 is almost done, 8 in progress

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -4,10 +4,7 @@ package lesson8.task1
 
 import lesson1.task1.sqr
 import java.lang.IllegalArgumentException
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
-import kotlin.math.sqrt
+import kotlin.math.*
 
 /**
  * Точка на плоскости
@@ -161,7 +158,13 @@ class Line private constructor(val b: Double, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point {
+        val dAngle = angle - other.angle
+        if (dAngle == 0.0) throw IllegalArgumentException("Прямые параллельны или совпадают")
+        val x = (other.b * cos(angle) - b * cos(other.angle)) / sin(dAngle)
+        val y = -(b * sin(other.angle) - other.b * sin(angle)) / sin(dAngle)
+        return Point(x, y)
+    }
 
     override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
 
@@ -179,7 +182,16 @@ class Line private constructor(val b: Double, val angle: Double) {
  *
  * Построить прямую по отрезку
  */
-fun lineBySegment(s: Segment): Line = TODO()
+fun lineBySegment(s: Segment): Line {
+    var alpha: Double = if (abs(s.begin.x - s.end.x) < Double.MIN_VALUE) { // Отрезок параллелен оси OY
+        PI / 2
+    } else {
+        atan((s.end.y - s.begin.y) / (s.end.x - s.begin.x))
+    }
+    if (alpha < 0) alpha += PI   // Приводим alpha в ОДЗ
+    if (alpha >= PI) alpha -= PI //
+    return Line(s.begin, alpha)
+}
 
 /**
  * Средняя
